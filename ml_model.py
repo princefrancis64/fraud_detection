1ba5638524aaa82df9a0cd2951bde85793f06ab4
princefrancis64 <prince.francis64@gmail.com>
2024-05-10 12:44:13 +0530

    added 3 new ml model codes

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 print("Adding all Ml related code here")
+
+
+def model1():
+    print("adding code for ML Model1")
+
+
+def model2():
+    print("adding code for ML Model2")
+
+def model3():
+    print("adding code for ML Model3")
