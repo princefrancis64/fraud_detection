4ea95f47e07b37b3ca053eb9cdc1e45952442556
princefrancis64 <prince.francis64@gmail.com>
2024-05-10 11:41:07 +0530

    added a wrong function

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,13 @@
 print("adding all the data processing code related to fraud detection")
 print('Hi')
+
+
+
+a = 5
+b = 7
+def sum(a,b):
+    c = a+b
+    return a
+
+
+print(f"added a function which is not correct: {sum(a,b)}")
